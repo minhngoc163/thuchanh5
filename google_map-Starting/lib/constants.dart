f1644a1d24c0ed92bd846ea444b5dd38afaa8408
Ngoc <ngocvo1603@gmail.com>
2023-08-17 14:40:06 +0700

    first commit

---
+++ google_map-Starting/lib/constants.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+const String google_api_key = "AIzaSyDj6PjV7l9WStaz_iQYdPZczdbLAT1_sQM";
+const Color primaryColor = Color(0xFF7B61FF);
+const double defaultPadding = 16.0;
